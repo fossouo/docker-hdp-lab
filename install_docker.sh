555ade044e03151e1c220cbfbf21b8ab619fd40a
root <root@dockerhost.home>
2017-11-19 04:45:32 +0100

    Added Install Docker Script

---
+++ install_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+########
+# Author: Donald FOSSOUO
+# Project: Docker HDP Lab
+# Description: The script will install Docker.
+#########
+
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
